e6d2665958527492d0f8b3f2bf3dada4e8baa81f
sequainoo <wazatoyung@gmail.com>
2022-02-16 15:40:31 +0000

    Fix post order

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - traverses tree inorder: like a sort
+ * binary_tree_postorder - traverses tree postorder:
  * @tree: the tree  t traverse
  * @func: oeration to perform on tree
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL)
 	{
-		binary_tree_inorder(tree->left, func);
+		binary_tree_postorder(tree->left, func);
 		func(tree->n);
-		binary_tree_inorder(tree->right, func);
+		binary_tree_postorder(tree->right, func);
 	}
 }
