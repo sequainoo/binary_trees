a30617645c41c2ee4c99044eccada7b1c2b3aae6
sequainoo <wazatoyung@gmail.com>
2022-06-04 19:39:02 +0000

    101

--- binary_trees.h
+++ binary_trees.h
@@ -52,5 +52,6 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
