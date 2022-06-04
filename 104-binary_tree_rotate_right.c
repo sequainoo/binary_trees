b56ac41d54b8588873e0262b12a29c76383498ba
sequainoo <wazatoyung@gmail.com>
2022-06-04 20:14:47 +0000

    Fix betty

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -7,19 +7,19 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-binary_tree_t *tmp, *aux;
+	binary_tree_t *tmp, *aux;
 
-tmp = NULL;
-if (tree == NULL || tree->left == NULL)
-return (NULL);
-if (tree->left->right != NULL)
-tmp = tree->left->right;
-aux = tree->left;
-tree->left->right = tree;
-tree->parent = tree->left;
-tree->left->parent = NULL;
-tree->left = tmp;
-if (tmp != NULL)
-tmp->parent = tree;
-return (aux);
+	tmp = NULL;
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
+	if (tree->left->right != NULL)
+		tmp = tree->left->right;
+		aux = tree->left;
+		tree->left->right = tree;
+		tree->parent = tree->left;
+		tree->left->parent = NULL;
+		tree->left = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	return (aux);
 }
