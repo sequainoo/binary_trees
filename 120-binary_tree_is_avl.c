b2ac26fc712741af661b870ee2ca0963cc50284a
sequainoo <wazatoyung@gmail.com>
2022-06-04 21:13:19 +0000

    120

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,15 +1,4 @@
 #include "binary_trees.h"
-
-/**
- * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
- *
- *@tree: pointer to the root node of the tree to check
- *
- * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
- */
-int binary_tree_is_avl(const binary_tree_t *tree)
-{
-	if (tree == NULL)#include "binary_trees.h"
 #include "limits.h"
 
 size_t height(const binary_tree_t *tree);
@@ -73,7 +62,4 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	return (is_avl_helper(tree, INT_MIN, INT_MAX));
-}
-	return (0);
-	return (1);
 }
